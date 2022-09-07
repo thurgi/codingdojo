4efc6c3afc20d4a5e397c66855008730dcd66249
Arrakis-Lux <arrakis.dc@gmail.com>
2022-09-07 14:23:27 +0200

    oubli hihi

--- pixelwarnode/src/controllers/pixel.controller.ts
+++ pixelwarnode/src/controllers/pixel.controller.ts
@@ -1,10 +1,17 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { UDPService } from 'src/services/UDPService/udp.service';
 import {Pixel} from '../models/pixel.model'
 
 
 @Controller('pixels')
 export class PixelController {
-  constructor() {}
+  constructor(private udpService: UDPService) {
+    udpService.listen('message').subscribe(this.onMessage);
+  }
+
+  onMessage(message) {
+    console.log('recieved message : ', message);
+  }
 
   @Get()
   getHello(): string {
