7d5adcd8ffa89d0a8474b19f85441564ccebaf3e
Anesca Frappas <anesca.frapaps.ext@orange.com>
2022-09-07 14:07:08 +0200

    thinkering with udpservice not working ATM

--- pixelwarnode/src/main.ts
+++ pixelwarnode/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+// import { UdpAdapter } from './adapters/udp.adapter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // const udpAdapter = new UdpAdapter(app);
+
+  // app.useWebSocketAdapter(udpAdapter);
   await app.listen(3000);
 }
 bootstrap();
